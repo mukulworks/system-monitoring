050f16847c39e3932443ebaedcfed760ec61aeee
Mukul Singh <orbitsyswork2023@gmail.com>
2024-01-31 16:19:40 +0530

    styling

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import Tab from 'react-bootstrap/Tab';
-import Tabs from 'react-bootstrap/Tabs';
-import './header.css';
+import React, { useState } from "react";
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
+import "./header.css";
 import { PROFILE_URL } from "../Routes/constant";
 
 const Header = () => {
@@ -12,164 +12,95 @@ const Header = () => {
     };
     return (
         <>
-            {/* <div className='header'>
-                <div className='dashboard'>
-                    <h1>Dashboards</h1>
-                </div>
-                <div className='account'>
-                    <h4>Your Name</h4>
-                    <h6>Designation</h6>
-                </div>
-                <div className='image'>
-                    <img
-                        src={`${PROFILE_URL}/profile.png`}
 
-                        alt="user profile"
-                    />
-                </div>
-
-            </div> */}
-            {/* <div className='response'>
-                <h5>System Monitoring</h5>
-            </div> */}
-
-            <div className='mn_hd'>
-
-                <div className='col-md-8'>
-                    <div className='des_tx'>
+            <div className="mn_hd">
+                <div className="col-md-8">
+                    <div className="des_tx">
                         <h1>Dashboards</h1>
 
-
-                        <div className='top_icon'>
+                        <div className="top_icon">
                             <ul>
                                 <li>
-
-                                    <a href=''>
+                                    <a href="">
                                         <img
                                             src={`${PROFILE_URL}/Circle-View.png`}
-
                                             alt="Circle View"
                                         />
                                     </a>
-
-
                                 </li>
                                 <li>
-
-                                    <a href=''>
+                                    <a href="">
                                         <img
                                             src={`${PROFILE_URL}/Long-Card-View.png`}
-
                                             alt="Circle View"
                                         />
                                     </a>
-
-
                                 </li>
 
                                 <li>
-
-                                    <a href=''>
+                                    <a href="">
                                         <img
                                             src={`${PROFILE_URL}/Card-View.png`}
-
                                             alt="Circle View"
                                         />
                                     </a>
-
-
                                 </li>
 
                                 <li>
-
-                                    <a href=''>
+                                    <a href="">
                                         <img
                                             src={`${PROFILE_URL}/Graph-View.png`}
-
                                             alt="Circle View"
                                         />
                                     </a>
-
-
                                 </li>
 
-                                <li className='icon_pdg1'>
-
-                                    <a href=''>
+                                <li className="icon_pdg1">
+                                    <a href="">
                                         <img
                                             src={`${PROFILE_URL}/Icon-awesome-star.png`}
-
                                             alt="Circle View"
                                         />
                                     </a>
-
-
                                 </li>
-
 
                                 <li>
-
-                                    <a href=''>
+                                    <a href="">
                                         <img
                                             src={`${PROFILE_URL}/Notification-Bell Icon.png`}
-
                                             alt="Circle View"
                                         />
                                     </a>
-
-
                                 </li>
-
                             </ul>
-
                         </div>
-
                     </div>
-
-
-
                 </div>
 
-                <div className='col-md-4'>
-                    <div className='user_nm'>
-
+                <div className="col-md-4">
+                    <div className="user_nm">
                         <p>Your Name</p>
                         <span>Designation</span>
-
-
-
                     </div>
 
-                    <div className='user_nm_pic'>
-                        <img
-                            src={`${PROFILE_URL}/user.png`}
-
-                            alt="user user"
-                        />
+                    <div className="user_nm_pic">
+                        <img src={`${PROFILE_URL}/user.png`} alt="user user" />
                     </div>
-
-
-
-
-
-
-
                 </div>
-
             </div>
 
-            <div className='system_tx'>
-
+            <div className="system_tx">
                 <h4>System Monitoring</h4>
-                <hr className='line1'></hr>
+                <hr className="line1"></hr>
             </div>
 
-
-
-            <div className={`w-100 sidebar-tabs d-flex justify-content-center align-items-center ${isToggleOn ? 'active' : ''}`} >
+            <div
+                className={`w-100 sidebar-tabs d-flex justify-content-center align-items-center ${isToggleOn ? "active" : ""
+                    }`}
+            >
                 <img
                     id="sidebar-click"
-                    className='me-2'
+                    className="me-2"
                     src={`${PROFILE_URL}/side_icon.png`}
                     width={50}
                     height={50}
@@ -179,11 +110,14 @@ const Header = () => {
                         handleToggle(); // Add this line to handle the click on the image
                     }}
                 />
-                <div className='sidebar-tabs-data'>
-                    <Tabs defaultActiveKey="profile" id="fill-tab-example" className="mb-3" fill>
-                        <Tab eventKey="home" title="Home">
-
-                        </Tab>
+                <div className="sidebar-tabs-data">
+                    <Tabs
+                        defaultActiveKey="profile"
+                        id="fill-tab-example"
+                        className="mb-3"
+                        fill
+                    >
+                        <Tab eventKey="home" title="Home"></Tab>
                         <Tab eventKey="profile" title="Profile">
                             Tab content for Profile
                         </Tab>
@@ -193,14 +127,7 @@ const Header = () => {
                     </Tabs>
                 </div>
             </div>
-
-
-
         </>
     );
-
-
-
-
-}
+};
 export default Header;
--- src/objects/Circles/circleobject.jsx
+++ src/objects/Circles/circleobject.jsx
@@ -1,6 +1,10 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { GetDashboardObject, GetObjectGroupQuery, API_HEADER } from "../../common/Routes/api_constant.jsx";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import {
+    GetDashboardObject,
+    GetObjectGroupQuery,
+    API_HEADER,
+} from "../../common/Routes/api_constant.jsx";
 import "./circleobject.css";
 
 const Props = localStorage.getItem("brand");
@@ -21,7 +25,8 @@ const api_parameters = {
 const formatDuration = (duration) => {
     const minutes = Math.floor(duration / 60);
     const seconds = duration % 60;
-    const minuteString = minutes > 0 ? `${minutes}m ${minutes > 1 ? "" : ""}` : "";
+    const minuteString =
+        minutes > 0 ? `${minutes}m ${minutes > 1 ? "" : ""}` : "";
     const secondString = seconds > 0 ? `${seconds}s${seconds > 1 ? "" : ""}` : "";
     if (minutes > 0) {
         return `${minuteString} ${secondString}`;
@@ -30,17 +35,18 @@ const formatDuration = (duration) => {
     }
 };
 
-
 const Circleobject = () => {
     const [object, setObject] = useState([]);
     const [countApiCall, setCountApiCall] = useState(false);
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await axios.post(api_url, api_parameters, { headers: api_header });
-                const data = response?.data?.result?.objectGroupDescList
+                const response = await axios.post(api_url, api_parameters, {
+                    headers: api_header,
+                });
+                const data = response?.data?.result?.objectGroupDescList;
                 setObject(data);
-                setCountApiCall(true)
+                setCountApiCall(true);
             } catch (error) { }
         };
 
@@ -53,42 +59,49 @@ const Circleobject = () => {
             CountryCode: "IN",
             ObjectGroup: "crm",
             QueryName: objectQuery,
-            OBJECTID: objectId
-        }
+            OBJECTID: objectId,
+        };
     };
     useEffect(() => {
-        object.flatMap(item => (
-            item.objectTypeList.flatMap((item1, index) => (
-                item1.objectDescList.flatMap((item2, index2) => (
-                    item2.dashboardObjectList.map(item3 => (
+        object.flatMap((item) =>
+            item.objectTypeList.flatMap((item1, index) =>
+                item1.objectDescList.flatMap((item2, index2) =>
+                    item2.dashboardObjectList.map((item3) =>
                         fetchData(index, index2, item3.objectId, item3.objectQuery)
-                    ))
-                ))
-            ))
-        ));
-
+                    )
+                )
+            )
+        );
     }, [countApiCall]);
 
     const fetchData = (index, index2, objectId, objectQuery) => {
         try {
-            const count = axios.post(api_query, query_parameters(objectId, objectQuery), { headers: api_header });
-            Promise.all([count])
-                .then((results) => {
-                    const countdata = results[0]?.data?.result?.[0]?.count;
-                    let temp = [...object];
-                    let tempObj = { ...temp[0]?.objectTypeList[index]?.objectDescList[index2] };
-                    tempObj.objectGroupShortDescName = countdata;
-                    temp[0]?.objectTypeList[index]?.objectDescList.splice(index2, 1, tempObj);
-                    setObject(temp);
-                })
-        } catch (error) {
-        }
+            const count = axios.post(
+                api_query,
+                query_parameters(objectId, objectQuery),
+                { headers: api_header }
+            );
+            Promise.all([count]).then((results) => {
+                const countdata = results[0]?.data?.result?.[0]?.count;
+                let temp = [...object];
+                let tempObj = {
+                    ...temp[0]?.objectTypeList[index]?.objectDescList[index2],
+                };
+                tempObj.objectGroupShortDescName = countdata;
+                temp[0]?.objectTypeList[index]?.objectDescList.splice(
+                    index2,
+                    1,
+                    tempObj
+                );
+                setObject(temp);
+            });
+        } catch (error) { }
     };
 
     return (
         <>
             {object.map((item) => (
-                <div className='background'>
+                <div className="background">
                     <div class="objgrpname">{item.objectGroupDescName}</div>
                     {item.objectTypeList.map((item1, index) => (
                         <>
@@ -99,22 +112,26 @@ const Circleobject = () => {
                                     <div class="flex-container">
                                         <div class="container">
                                             <div class="circle">
-                                                <div class="interval">{formatDuration(item2.refreshIntervals)}</div>
-                                                <div class="count">{item2?.objectGroupShortDescName != undefined ? item2?.objectGroupShortDescName : 0}</div>
+                                                <div class="interval">
+                                                    {formatDuration(item2.refreshIntervals)}
+                                                </div>
+                                                <div class="count">
+                                                    {item2?.objectGroupShortDescName != undefined
+                                                        ? item2?.objectGroupShortDescName
+                                                        : 0}
+                                                </div>
                                             </div>
                                             <div class="objectname">{item2.objectDescName}</div>
                                         </div>
                                     </div>
                                 </>
-
                             ))}
                         </>
                     ))}
-
-                </div >
+                </div>
             ))}
         </>
     );
-}
+};
 
 export default Circleobject;
